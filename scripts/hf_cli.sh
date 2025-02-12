f2ca5857f3d27b1366ea4a750bfd68710887be90
JianZhengNie <jianzhnie@126.com>
2025-02-12 13:09:37 +0800

    Update openr1

---
+++ scripts/hf_cli.sh
@@ -0,0 +1,9 @@
+
+# download  dataset
+huggingface-cli download  --repo-type dataset bespokelabs/Bespoke-Stratos-17k  --local-dir /home/robin/hf_hub/datasets/text_data/bespokelabs/Bespoke-Stratos-17k
+huggingface-cli download  --repo-type dataset HuggingFaceH4/Bespoke-Stratos-17k  --local-dir /home/robin/hf_hub/datasets/text_data/HuggingFaceH4/Bespoke-Stratos-17k
+
+
+# download  model
+huggingface-cli download  Qwen/Qwen2.5-0.5B  --local-dir /home/robin/hf_hub/models/Qwen/Qwen2.5-0.5B
+huggingface-cli download  Qwen/Qwen2.5-1.5B-Instruct  --local-dir /home/robin/hf_hub/models/Qwen/Qwen2.5-1.5B-Instruct
